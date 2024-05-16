616e830da91b61ac921bfd633ea7d1f6e03479bb
takagi2943 <154996725+takagi2943@users.noreply.github.com>
2024-05-16 17:40:00 +0900

    Initial Commit

---
+++ sample.rb
@@ -0,0 +1,32 @@
+puts 1 + 2
+
+a = 'Hello, world!'
+puts a
+
+b = 'こんにちは'
+puts b
+
+# data = find_data
+#     if data
+#         'データがあります'
+#     else
+#         'データはありません'
+#     end 
+
+# country = 'italy'
+# if country == 'japan'
+#     puts 'こんにちは'
+# elsif country == 'us'
+#     puts 'Hello'
+# elsif country == 'italy'
+#     puts 'Ciao'
+# else
+#     puts '???'
+# end
+
+point = 7
+day = 1
+if day == 1
+    point *= 5
+end
+point
