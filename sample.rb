8cb8bfbbad4b3d45b237f196e75734ca5f8c3390
takagi2943 <154996725+takagi2943@users.noreply.github.com>
2024-05-19 00:24:26 +0900

    ruby 復習ブロック

--- sample.rb
+++ sample.rb
@@ -1,10 +1,10 @@
-puts 1 + 2
+# puts 1 + 2
 
-a = 'Hello, world!'
-puts a
+# a = 'Hello, world!'
+# puts a
 
-b = 'こんにちは'
-puts b
+# b = 'こんにちは'
+# puts b
 
 # data = find_data
 #     if data
@@ -31,17 +31,17 @@ puts b
 # end
 # point
 
-def fizz_buzz(n)
-  if n % 15 == 0
-    'FIizz Buzz'
-  elsif n % 3 == 0
-    'Fizz'
-  elsif n % 5 == 0
-    'Buzz'
-  else
-    n.to_s
-  end 
-end 
+# def fizz_buzz(n)
+#   if n % 15 == 0
+#     'FIizz Buzz'
+#   elsif n % 3 == 0
+#     'Fizz'
+#   elsif n % 5 == 0
+#     'Buzz'
+#   else
+#     n.to_s
+#   end 
+# end 
 
 # require 'minitest/autorun'
 
@@ -51,11 +51,37 @@ end
 #     assert_equal '2', fizz_buzz(2)
 #     assert_equal 'Fizz', fizz_buzz(3)
 
-puts fizz_buzz(1)
-puts fizz_buzz(2)
-puts fizz_buzz(3)
-puts fizz_buzz(4)
-puts fizz_buzz(5)
-puts fizz_buzz(6)
-puts fizz_buzz(15)
+# puts fizz_buzz(1)
+# puts fizz_buzz(2)
+# puts fizz_buzz(3)
+# puts fizz_buzz(4)
+# puts fizz_buzz(5)
+# puts fizz_buzz(6)
+# puts fizz_buzz(15)
 
+# a,b = 1,2
+
+# puts a
+# puts b
+
+# c, d = [10]
+# puts c  # 10
+# puts d.inspect # nil
+
+# 14.divmod(3)
+
+# quo_rem = 14.divmod(3)
+# puts "商=#{quo_rem[0]}, 余り=#{quo_rem[1]}"
+
+# numbers = [1, 2, 3, 4,]
+# sum = 0
+# numbers.each do |n|
+#   sum += n
+# end 
+# puts sum 
+
+a = [1, 2, 3, 1, 2, 3]
+# a.delete_if do |n|
+  a.delete(2)
+# end 
+puts a
